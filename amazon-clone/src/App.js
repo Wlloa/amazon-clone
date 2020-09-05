335b7499b416b5829c561028f7ad27bd579d7d4b
Wilber Ulloa <wuj890312@gmail.com>
2020-09-05 16:48:21 -0400

    updating home

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+        <Router>
+          <Switch>
+            <Route path="/" component={Home} exact></Route>
+            <Route path='/checkout'>Chekout page</Route>
+            <Route path='/login'>Login page</Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ amazon-clone/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../image/2375B_Banner1_WAVE_2048x497.jpg';
+
+const Home = ()=>{
+    return(
+        <div className="home">
+            <div className="home__banner">
+                <img src="" alt="" className="home__banner__img"/>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
